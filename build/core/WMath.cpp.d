1ab3f14432b43f3586a0b62c00474f6c4cc37eb1
barrack2018 <joseestebanojedaaular@gamil.com>
2022-05-21 00:34:24 -0400

    teseteo de intelisence(borrado de datos iniciales)

--- build/core/WMath.cpp.d
+++
@@ -1,2 +0,0 @@
-c:\Users\josee\Documents\Arduino\Bluetooth\BTLED_PORJECT\build\core\WMath.cpp.o: \
- C:\Users\josee\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino\WMath.cpp
--- build/sketch/README.md
+++
@@ -1,2 +0,0 @@
-#line 1 "c:\\Users\\josee\\Documents\\Arduino\\Bluetooth\\BTLED_PORJECT\\README.md"
-# BTLED_PORJECT
