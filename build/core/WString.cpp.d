08ddfb3c0861134e557837b0d1599e1960c7b97c
barrack2018 <joseestebanojedaaular@gamil.com>
2022-05-22 07:58:50 -0400

    eliminacion de la capeta build

--- build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-c:\Users\josee\Documents\Arduino\Bluetooth\BTLED_PORJECT\build\core\WString.cpp.o: \
- C:\Users\josee\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino\WString.cpp \
- C:\Users\josee\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino\WString.h
--- build/core/wiring_digital.c.d
+++
@@ -1,6 +0,0 @@
-c:\Users\josee\Documents\Arduino\Bluetooth\BTLED_PORJECT\build\core\wiring_digital.c.o: \
- C:\Users\josee\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino\wiring_digital.c \
- C:\Users\josee\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino\wiring_private.h \
- C:\Users\josee\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino\Arduino.h \
- C:\Users\josee\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino\binary.h \
- C:\Users\josee\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\variants\mega/pins_arduino.h
